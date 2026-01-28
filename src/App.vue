6ace72da0ad8ecc802697b79f4f71dd531b33f54
Sedres <sedres13@protonmail.com>
2026-01-28 18:27:42 +0100

    docs: Added some assets, set background and little loader

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup></script>
 
 <template>
+  <img class="background" src="@/assets/images/background.webp"></img>
   <section id="navbar"></section>
   <section id="about"></section>
   <section id="projects"></section>
   <section id="contact"></section>
 </template>
 
-<style scoped></style>
+<style>
+  .background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: -1;
+  }
+</style>
